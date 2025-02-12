f88275c4aebdcaaf325e365e8dd33785b93ba715
Mohammed_ispro <alballamohammed49@gmail.com>
2025-02-12 21:06:34 +0400

    Start up

---
+++ resources/js/Pages/Dashboard.tsx
@@ -0,0 +1,23 @@
+import { Input } from '@/components/ui/input';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Dashboard() {
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    Dashboard
+                </h2>
+            }
+        >
+            <Head title="Dashboard" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 text-white">
+                    
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
